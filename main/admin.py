e931fa06e120d6881bdf86701f80f138d0975d95
mzazakeith <mzazakeith@gmail.com>
2018-08-24 10:33:50 +0300

    models added in admin for testing

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from main.models import Curriculum, SLTASpot
+from main.models import Curriculum, SLTASpot, PatientProfile, AssistantProfile
 from leaflet.admin import LeafletGeoAdmin
 
 admin.site.register(Curriculum)
 admin.site.register(SLTASpot, LeafletGeoAdmin)
+admin.site.register(PatientProfile)
+admin.site.register(AssistantProfile)
